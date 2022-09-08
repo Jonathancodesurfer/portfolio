0a8a4a6226b0fff6ebeb01849c01c0f035aa4a9f
Jonathancodesurfer <jonathan@ivcmexico.com>
2022-09-08 11:15:31 -0600

    changed Formik to html form

--- src/components/FormikForm.js
+++ src/components/FormikForm.js
@@ -1,126 +1,26 @@
 import './Formik.css';
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Formik, Form, useField } from 'formik';
-import * as Yup from 'yup';
 
-const MyTextInput = ({ label, ...props }) => {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input>. We can use field meta to show an error
-  // message if the field is invalid and it has been touched (i.e. visited)
-  const [field, meta] = useField(props);
+
+
+const ContactForm = () => {
+
+    
+
   return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
+      <div>
+        <form id='contactForm' name="contact" method="POST" data-netlify="true">
+              <input  placeholder='First Name' type="text" name="name" />
+              <input  placeholder='Last Name' type="text" name="last name" />
+              <input  placeholder='Email' type="email" name="email" />
+              <textarea placeholder='Message' name="message"></textarea>
+              <button type="submit">Send</button>
+              
+        </form>
+      </div>
+    
 
-const MyCheckbox = ({ children, ...props }) => {
-  // React treats radios and checkbox inputs differently other input types, select, and textarea.
-  // Formik does this too! When you specify `type` to useField(), it will
-  // return the correct bag of props for you -- a `checked` prop will be included
-  // in `field` alongside `name`, `value`, `onChange`, and `onBlur`
-  const [field, meta] = useField({ ...props, type: 'checkbox' });
-  return (
-    <div>
-      <label className="checkbox-input">
-        <input type="checkbox" {...field} {...props} />
-        {children}
-      </label>
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </div>
-  );
-};
-
-const MySelect = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
-  return (
-    <div>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <select {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </div>
-  );
-};
-
-const MyTextArea = ({ label, ...props }) => {
-    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-    // which we can spread on <input>. We can use field meta to show an error
-    // message if the field is invalid and it has been touched (i.e. visited)
-    const [field, meta] = useField(props);
-    return (
-      <>
-        <textarea className="text-input" {...field} {...props} />
-        {meta.touched && meta.error ? (
-          <div className="error">{meta.error}</div>
-        ) : null}
-      </>
-    );
-  };
-
-// And now we can use these
-const SignupForm = () => {
-  return (
-    <div className='Form_container'>
-      <h1 className='Form_name' style={{textAlign:"center"}}>Contact Us!</h1>
-      <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: ''
-        }}
-        validationSchema={Yup.object({
-          email: Yup.string()
-            .email('Invalid email address')
-            .required('Required')
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        <Form name="contact" method="POST" data-netlify="true">
-          <MyTextInput
-            name="firstName"
-            type="text"
-            placeholder="First Name"
-          />
-
-          <MyTextInput
-            name="lastName"
-            type="text"
-            placeholder="Last Name"
-          />
-
-          <MyTextInput
-            name="email"
-            type="email"
-            placeholder="Email Address"
-          />
-
-          <MyTextArea 
-            name="message"
-            type="message"
-            placeholder="Message"
-          />
-
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </div>
   );
 };
  
-export default SignupForm;
+export default ContactForm;
