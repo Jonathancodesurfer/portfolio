9c90144856c9a7c393348665f422f1160f9f2e7d
Jonathancodesurfer <jonathan@ivcmexico.com>
2022-09-09 16:13:24 -0600

    styled form

--- src/components/FormikForm.js
+++ src/components/FormikForm.js
@@ -9,13 +9,13 @@ const ContactForm = () => {
 
   return (
       <div className='Form_container'>
-          <h1 className='Form_title'>Contact us</h1>
+        <h1 className='Form_title'>Contact us</h1>
         <form  id='contactForm' name="contact" method="POST" data-netlify="true">
               <input type="hidden" name="form-name" value="contact" />
               <input  placeholder='First Name' type="text" name="name" />
               <input  placeholder='Last Name' type="text" name="last name" />
-              <input  placeholder='Email' type="email" name="email" />
-              <textarea placeholder='Message' name="message"></textarea>
+              <input  placeholder='Email' type="text" name="email" />
+              <textarea placeholder='Message' type="text" name="message"></textarea>
               <button type="submit">Send</button>
               
         </form>
