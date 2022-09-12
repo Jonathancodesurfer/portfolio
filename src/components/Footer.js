0ed4257d9f0577183daa1e383b9b297f124fd86e
Jonathancodesurfer <jonathan@ivcmexico.com>
2022-09-12 12:19:25 -0600

    added socil icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import logo from './assets/logo.svg';
+import github from './assets/github.svg';
+import linkedin from './assets/linkedin-circled.svg';
 import './Footer.css';
 
 const Footer = (props) => {
@@ -10,9 +12,9 @@ const Footer = (props) => {
                     <Link to="/"><img width={80} src={logo}></img></Link> 
                 </div>
                 <div>
-                    <a href='https://github.com/Jonathancodesurfer'><img width={40} src={logo}></img></a>
-                    <a href='https://github.com/Jonathancodesurfer'><img width={40} src={logo}></img></a>
-                    <a href='https://github.com/Jonathancodesurfer'><img width={40} src={logo}></img></a>
+                    <a href='https://github.com/Jonathancodesurfer' target="_blank"><img width={40} src={github}></img></a>
+                    <a href='https://www.linkedin.com/in/jonathan-alpizar-9697a7205/' target="_blank"><img width={40} src={linkedin}></img></a>
+                  
                 </div>
                 <div className='Footer_links'>
                     <Link className='Navbar_mobile-menu-items' to='/'>Home</Link>
